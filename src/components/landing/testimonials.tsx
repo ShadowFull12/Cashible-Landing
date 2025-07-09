11220e5b0f384adde5d39ae60f85040999964a87
Shadow Full <shadowfull321@gmail.com>
2025-07-09 16:16:15 +0000

    actually make the arrows disappear in the testimonials section please in

--- src/components/landing/testimonials.tsx
+++ src/components/landing/testimonials.tsx
@@ -106,8 +106,8 @@ export function Testimonials() {
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="absolute left-2 md:left-0 md:-translate-x-1/2 top-1/2 -translate-y-1/2 z-10 h-10 w-10 md:h-12 md:w-12 bg-background/50 backdrop-blur-md hover:bg-primary/20 flex"/>
-          <CarouselNext className="absolute right-2 md:right-0 md:translate-x-1/2 top-1/2 -translate-y-1/2 z-10 h-10 w-10 md:h-12 md:w-12 bg-background/50 backdrop-blur-md hover:bg-primary/20 flex"/>
+          <CarouselPrevious className="absolute left-2 md:left-0 md:-translate-x-1/2 top-1/2 -translate-y-1/2 z-10 h-10 w-10 md:h-12 md:w-12 bg-background/50 backdrop-blur-md hover:bg-primary/20 hidden md:flex"/>
+          <CarouselNext className="absolute right-2 md:right-0 md:translate-x-1/2 top-1/2 -translate-y-1/2 z-10 h-10 w-10 md:h-12 md:w-12 bg-background/50 backdrop-blur-md hover:bg-primary/20 hidden md:flex"/>
         </Carousel>
       </div>
     </section>
